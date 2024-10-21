fb85adc112b1df9e0af02e81d70ff77209b652c0
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-10-21 16:43:35 +0700

    Initial Commit

---
+++ Movie-Catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Catalog
+//
+//  Created by Богдан Тарченко on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
