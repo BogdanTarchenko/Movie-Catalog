0062104fd695b3fcbd84703187e5d662211c44f6
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-10-21 17:11:23 +0700

    Clean Architecture pattern added

---
+++ Movie-Catalog/Presentation/Routers/AppRouter.swift
@@ -0,0 +1,20 @@
+//
+//  AppRouter.swift
+//  Movie-Catalog
+//
+//  Created by Богдан Тарченко on 21.10.2024.
+//
+
+import UIKit
+import KeychainAccess
+
+class AppRouter {
+    
+    private var window: UIWindow?
+    
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    func start() {}
+}
